32b41933592b410941f03592426f5b3879a11159
Constadine <conpap95@live.com>
2021-05-27 00:14:48 +0300

    Added Lessons

--- Projects/DATAPROJECT/main.py
+++ Projects/DATAPROJECT/main.py
@@ -1,6 +1,7 @@
-from menus import print_menu, print_submenu
+from menus import *
 from pupils import Pupils
 from teachers import Teachers
+from lessons import Lessons
 
 
 def get_int(prompt):
@@ -18,138 +19,166 @@ def main():
 
     pupils = Pupils()
     teachers = Teachers()
+    lessons = Lessons()
 
     while True:
-        print_menu()
+
+        menu()
+
         choice = get_int("Pick one: ")
 
         if choice == 1:
-            print("NEW PUPIL")
-            print("==========")
-            pupil = pupils.create_pupil()
-            if pupil is None:
-                continue
-            else:
-                print("NEW PUPIL CREATED")
-                print(pupil)
 
-        elif choice == 2:
-            print_submenu()
-            print_choice = get_int("Give your choice: ")
+            pupils_menu()
 
-            if print_choice == 1:
-                pupil_id = get_int("Give id:")
-                pupil = pupils.search_pupil_by_id(pupil_id)
+            pupils_choice = get_int("Pick one: ")
+
+            if pupils_choice == 1:
+                print("NEW PUPIL")
+                print("===========")
+                pupil = pupils.create_pupil()
                 if pupil is None:
-                    print("Pupil  with this id does not exist.")
+                    continue
                 else:
-                    print("   PUPIL   ")
+                    print("NEW PUPIL")
                     print(pupil)
 
-            elif print_choice == 2:
-                print("\n")
-                print(pupils)
+            elif pupils_choice == 2:
 
-            elif print_choice == 3:
-                print("\n")
-                pupils.print_pupils_names()
+                pupils_print_submenu()
 
-            else:
-                print("Wrong input! ")
-                continue
+                print_choice = get_int("Give your choice: ")
+
+                if print_choice == 1:
+                    pupil_id = get_int("Give id: ")
+                    pupil = pupils.search_pupil_by_id(pupil_id)
+                    if pupil is None:
+                        print("Pupil does not exist (with this id)")
+                    else:
+                        print("   PUPIL   ")
+                        print(pupil)
+                elif print_choice == 2:
+                    print(pupils)
+                elif print_choice == 3:
+                    pupils.print_pupils_names()
+                else:
+                    print("Wrong input! ")
+                    continue
+
+            elif pupils_choice == 3:
+
+                pupils_update_submenu()
+
+                update_choice = get_int("Give your choice: ")
+                if update_choice == 1:
+                    pupil_id = get_int("Give id: ")
+                    pupil = pupils.search_pupil_by_id(pupil_id)
+                    if pupil is None:
+                        print("Error! There is no pupil with this id!")
+                        continue
+                elif update_choice == 2:
+                    surname = input("Give surname: ")
+                    matching_pupils = pupils.search_pupil_by_surname(surname)
+                    if not matching_pupils:
+                        print("No matching pupils with this surname!")
+                        continue
+                    elif len(matching_pupils) == 1:
+                        pupil = matching_pupils[0]
+                    else:
+                        for p in matching_pupils:
+                            print(p)
+                            print(f"id = {p['id']}")
+                            print("-" * 15)
+                        pupil_id = get_int("Give id: ")
+                        pupil = pupils.search_pupil_by_id(pupil_id)
+
+                # pupil: update
+                pupils.pupil_update(pupil)
+
+            elif pupils_choice == 4:
+
+                pupils_delete_submenu()
+
+                delete_choice = get_int("Give your choice: ")
+
+                if delete_choice == 1:
+                    pupil_id = get_int("Give id: ")
+
+                    pupil = pupil.search_pupil_by_id(pupil_id)
+                    if pupil is None:
+                        print("Error! No pupil with this id!")
+                        continue
+
+                elif delete_choice == 2:
+                    last_name = input("Give surname: ")
+                    matching_pupils = pupils.search_pupil_by_surname(last_name)
+                    if not matching_pupils:
+                        print("No matching pupils with this surname!")
+                        continue
+                    elif len(matching_pupils) == 1:
+                        pupil = matching_pupils[0]
+                    else:
+                        for p in matching_pupils:
+                            print(p)
+                            print(f"id = {p['id']}")
+                            print("-" * 15)
+                        pupil_id = get_int("Give id: ")
+                        pupil = pupils.search_pupil_by_id(pupil_id)
+
+                pupils.delete_pupil_by_id(pupil_id)
+
+        elif choice == 2:
+
+            teachers_menu()
+
+            teacher_choice = get_int("Pick one: ")
+
+            if teacher_choice == 1:
+                first_name = input("Give name: ")
+                last_name = input("Give surname: ")
+                teachers.create_teacher(first_name, last_name)
+            elif teacher_choice == 2:
+                teacher_id = get_int("Give id: ")
+                teacher = teachers.read_teacher(teacher_id)
+                if teacher is None:
+                    print("No such teacher exists!")
+                else:
+                    print(teacher)
+            elif teacher_choice == 3:
+                teacher_id = get_int("Give id: ")
+                teachers.update_teacher(teacher_id)
+            elif teacher_choice == 4:
+                teacher_id = get_int("Give id: ")
+                teachers.delete_teacher(teacher_id)
 
         elif choice == 3:
-            print("\n===============")
-            print("      SUB-MENU (UPDATE) ")
-            print("1 - Update Pupil (search by id)")
-            print("2 - Update Pupil (search by surname)")
-            update_choice = get_int("Give your choice: ")
 
-            if update_choice == 1:
-                pupil_id = get_int("Give id: ")
+            lessons_menu()
 
-                pupil = pupils.search_pupil_by_id(pupil_id)
-                if pupil is None:
-                    print("Error! No pupil with this id!")
-                    continue
+            lesson_choice = get_int("Pick one: ")
 
-            elif update_choice == 2:
-                last_name = input("Give surname: ")
-                matching_pupils = pupil.search_pupil_by_surname(last_name)
-                if not matching_pupils:
-                    print("No matching pupils with this surname!")
-                    continue
-                elif len(matching_pupils) == 1:
-                    pupil = matching_pupils[0]
+            if lesson_choice == 1:
+                lesson_name = input("Give lesson name: ")
+                lessons.create_lesson(lesson_name)
+            elif lesson_choice == 2:
+                lesson_id = get_int("Give id: ")
+                lesson = lessons.read_lesson(lesson_id)
+                if lesson is None:
+                    print("No such lesson exists!")
                 else:
-                    for p in matching_pupils:
-                        print(p)
-                        print(f"id = {p['id']}")
-                        print("-" * 15)
-                    pupil_id = get_int("Give id: ")
-                    pupil = pupils.search_pupil_by_id(pupil_id)
-
-            pupils.pupil_update(pupil)
+                    lesson.print_lesson_details(teachers, pupils)
+            elif lesson_choice == 3:
+                lesson_id = get_int("Give id: ")
+                lessons.update_lesson(lesson_id, teachers, pupils)
+            elif lesson_choice == 4:
+                lesson_id = get_int("Give id: ")
+                lessons.delete_lesson(lesson_id)
 
         elif choice == 4:
-            print("\n===============")
-            print("      SUB-MENU (DELETE) ")
-            print("1 - Delete Pupil (search by id)")
-            print("2 - Delete Pupil (search by surname)")
-            delete_choice = get_int("Give your choice: ")
-
-            if delete_choice == 1:
-                pupil_id = get_int("Give id: ")
-
-                pupil = pupil.search_pupil_by_id(pupil_id)
-                if pupil is None:
-                    print("Error! No pupil with this id!")
-                    continue
-
-            elif delete_choice == 2:
-                last_name = input("Give surname: ")
-                matching_pupils = pupils.search_pupil_by_surname(last_name)
-                if not matching_pupils:
-                    print("No matching pupils with this surname!")
-                    continue
-                elif len(matching_pupils) == 1:
-                    pupil = matching_pupils[0]
-                else:
-                    for p in matching_pupils:
-                        print(p)
-                        print(f"id = {p['id']}")
-                        print("-" * 15)
-                    pupil_id = get_int("Give id: ")
-                    pupil = pupils.search_pupil_by_id(pupil_id)
-
-            pupils.delete_pupil_by_id(pupil["id"])
-
-        elif choice == 5:
-            first_name = input("Give name: ")
-            surname = input("Give surname: ")
-            teachers.create_teacher(first_name, surname)
-            print(teachers)
-
-        elif choice == 6:
-            teacher_id = get_int("Give id: ")
-            teacher = teachers.read_teacher(teacher_id)
-            if teacher is None:
-                print("No such teacher exists!")
-            else:
-                print(teacher)
-
-        elif choice == 7:
-            teacher_id = get_int("Give id: ")
-            teachers.update_teacher(teacher_id)
-
-        elif choice == 8:
-            teacher_id = get_int("Give id: ")
-            teachers.delete_teacher(teacher_id)
-
-        elif choice == 9:
-            print("Bye.")
-            teachers.save_teachers_data()
+            print("Bye bye!")
             pupils.save_pupils_data()
+            teachers.save_teachers_data()
+            lessons.save_lessons_data()
             break
 
 
--- Projects/DATAPROJECT/menus.py
+++ Projects/DATAPROJECT/menus.py
@@ -1,22 +1,61 @@
 
-def print_menu():
+def menu():
     print("\n===============")
-    print("      MENU      ")
+    print("     MENU ")
+
+    print("1 - Manage Pupils")
+    print("2 - Manage Teachers")
+    print("3 - Manage Lessons")
+    print("4 - Exit")
+
+
+def pupils_menu():
+    print("\n===============")
+    print(" PUPILS MENU ")
+
     print("1 - Create Pupil")
-    print("2 - Print")
+    print("2 - Print Pupil(s)")
     print("3 - Update Pupil")
     print("4 - Delete Pupil")
-    print("5 - Create Teacher")
-    print("6 - Read Teacher")
-    print("7 - Update Teacher")
-    print("8 - Delete Teacher")
-
-    print("9 - Exit")
 
 
-def print_submenu():
+def pupils_print_submenu():
     print("\n===============")
-    print("      SUB-MENU (PRINT) ")
+    print("     SUB-MENU (PRINT) ")
     print("1 - Print Pupil")
     print("2 - Print all pupils (details)")
-    print("3 - Print all pupils (names)")
+    print("3 - Print all pupils (just the names)")
+
+
+def pupils_update_submenu():
+    print("\n===============")
+    print("     SUB-MENU (UPDATE) ")
+    print("1 - Update Pupil (search by id)")
+    print("2 - Update Pupil (search by surname)")
+
+
+def pupils_delete_submenu():
+    print("\n===============")
+    print("     SUB-MENU (DELETE) ")
+    print("1 - Delete Pupil (search by id)")
+    print("2 - Delete Pupil (search by surname)")
+
+
+def teachers_menu():
+    print("\n===============")
+    print(" TEACHERS MENU ")
+
+    print("1 - Create Teacher")
+    print("2 - Print Teacher(s)")
+    print("3 - Update Teacher")
+    print("4 - Delete Teacher")
+
+
+def lessons_menu():
+    print("\n===============")
+    print(" LESSONS MENU ")
+
+    print("1 - Create Lesson")
+    print("2 - Print Lesson")
+    print("3 - Update Lesson")
+    print("4 - Delete Lesson")
